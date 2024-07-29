6d38f42672e43e9ab8f63e35502e32d9e8b8e285
zainabkarim18 <zainabkarim18@github.com>
2024-07-29 14:36:23 +0300

    zee2 imp

--- server.js
+++ server.js
@@ -2,21 +2,51 @@ require('dotenv').config();
 const express = require('express');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const isSignedIn = require('./middleware/is-signed-in');
+const passUserToView = require('./middleware/pass-user-to-view');
+
 
 // DATABASE
 require('./config/db');
 
 const app = express();
 
+// CONTROLLERS
+const movieCtrl = require('./controllers/movie');
+const userCtrl = require('./controllers/user');
+
 // MIDDLEWARE
-app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGODB_URL,
+        }),
+    })
+);
 
+// app.use(passUserToView);
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+app.get('/vip-lounge', isSignedIn, (req, res, next) => {
+    res.send(`Welcome to the party ${req.session.user.username}.`);
 });
+
+// ROUTES
+app.use('/movies', movieCtrl);
+app.use('/users', userCtrl);
+
+app.get('/', (req, res, next) => {
+    const user = req.session.user;
+    res.render('index.ejs');
+});
+
 
 
 app.listen(process.env.PORT, () => {
